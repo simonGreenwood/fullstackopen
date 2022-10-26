37d6d1929e62339e03ef06b147c347a3e0bc3f9a
Simon Greenwood <simonrobertgreenwood@gmail.com>
2022-10-26 15:30:32 +0100

    added blog router

--- part4/index.js
+++ part4/index.js
@@ -4,29 +4,13 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const Blog = require('./models/Blog')
+const blogRouter = require('./controllers/blog')
 const mongoUrl = 'mongodb://127.0.0.1:27017/fullstack-blog'
 mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
