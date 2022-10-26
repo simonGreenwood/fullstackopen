9158b3579fd88ef75607847716632eac0ac49d82
Simon Greenwood <simonrobertgreenwood@gmail.com>
2022-10-26 15:24:21 +0100

    added blog model

--- part4/index.js
+++ part4/index.js
@@ -3,16 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
+const Blog = require('./models/Blog')
 const mongoUrl = 'mongodb://127.0.0.1:27017/fullstack-blog'
 mongoose.connect(mongoUrl)
 
