c7865725efe57602d8f6b0e59fcc79545180ccae
Simon Greenwood <simonrobertgreenwood@gmail.com>
2022-12-11 08:32:02 +0000

    exercise 5.5

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 
 import Blog from './components/Blog'
 import Notification from './components/Notification'
+import BlogForm from './components/BlogForm'
+import Togglable from './components/Togglable'
 
 import blogService from './services/blogs'
 import loginService from './services/login'
@@ -16,42 +18,10 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
   const [success, setSuccess] = useState(null)
 
-
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
-
   const handleLogout = () => {
     console.log("logging out")
     window.localStorage.removeItem('loggedBlogappUser')
   }
-  const handleCreateBlog = async (event) => {
-    event.preventDefault()
-    const blogObject = {
-      title: title,
-      author: author,
-      url: url,
-    }
-    console.log(blogObject)
-    try {
-      const returnedBlog = await blogService.createBlog(blogObject)
-      console.log('created blog')
-      setBlogs(blogs.concat(returnedBlog))
-      setErrorMessage('created blog')
-      setSuccess(true)
-      setTimeout(() => {
-        setErrorMessage(null)
-      },5000)
-    } catch (exception) {
-      console.log('invalid content')
-      setErrorMessage('invalid content')
-      setSuccess(false)
-      setTimeout(() => {
-        setErrorMessage(null)
-      },5000)
-    }
-  }
-
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -124,41 +94,10 @@ const App = () => {
       <div>
         {user.name} logged in <button onClick={() => handleLogout()}>logout</button>
       </div>
-      <Notification message={errorMessage} success={success}/>
-      <div>
-        <h2>create new</h2>
-        <form>
-          <div>
-            title:
-            <input
-              type="text"
-              value={title}
-              name="title"
-              onChange={({ target }) => setTitle(target.value)}
-            />
-          </div>
-          <div>
-            author:
-            <input
-              type="text"
-              value={author}
-              name="author"
-              onChange={({ target }) => setAuthor(target.value)}
-            />
-          </div>
-          <div>
-            url:
-            <input
-              type="text"
-              value={url}
-              name="url"
-              onChange={({ target }) => setUrl(target.value)}
-            />
-          </div>
-          <button onClick={(event) => handleCreateBlog(event)}>create</button>
-        </form> 
-      </div> 
-    
+      <Notification message={errorMessage} success={success} /> 
+      <Togglable buttonLabel="new blog">
+        <BlogForm setErrorMessage={setErrorMessage} setSuccess={setSuccess} setBlogs={setBlogs} blogs={blogs}/>
+      </Togglable>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
---
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -0,0 +1,73 @@
+import { useState } from "react"
+import blogService from "../services/blogs"
+
+const BlogForm = ({ setErrorMessage, setSuccess, blogs, setBlogs }) => {
+
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const handleCreateBlog = async (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: title,
+      author: author,
+      url: url,
+    }
+    console.log(blogObject)
+    try {
+      const returnedBlog = await blogService.createBlog(blogObject)
+      console.log('created blog')
+      setBlogs(blogs.concat(returnedBlog))
+      setErrorMessage('created blog')
+      setSuccess(true)
+      setTimeout(() => {
+        setErrorMessage(null)
+      },5000)
+    } catch (exception) {
+      console.log('invalid content')
+      setErrorMessage('invalid content')
+      setSuccess(false)
+      setTimeout(() => {
+        setErrorMessage(null)
+      },5000)
+    }
+  }
+  
+  return (
+    <div>
+      <h2>create new</h2>
+      <form>
+        <div>
+          title:
+          <input
+            type="text"
+            value={title}
+            name="title"
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author:
+          <input
+            type="text"
+            value={author}
+            name="author"
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url:
+          <input
+            type="text"
+            value={url}
+            name="url"
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button onClick={(event) => handleCreateBlog(event)}>create</button>
+      </form> 
+    </div> 
+  )
+}
+export default BlogForm
