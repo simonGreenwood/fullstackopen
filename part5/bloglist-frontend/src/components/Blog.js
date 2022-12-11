0a879952bb462d973b5a1b692e0e08123b6d8484
Simon Greenwood <simonrobertgreenwood@gmail.com>
2022-12-11 08:48:59 +0000

    exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import Togglable from "./Togglable"
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [extended, setExtended] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const handleLike = () => {
+    console.log('like')
+  }
+  const handleView = () => {
+    setExtended(!extended)
+  }
+  return (
+    <div style={blogStyle}>
+      <div>{blog.title} <button onClick={() => handleView()}>{extended ? "view" : "hide"}</button></div>
+      {extended ? 
+        <div>
+          <div>{blog.url}</div>
+          <div>likes: {blog.likes} <button onClick={() => handleLike()}>like</button></div>
+          <div>{blog.author}</div>
+        </div> 
+      : null}
+    </div>  
+  )
+}
 
 export default Blog
