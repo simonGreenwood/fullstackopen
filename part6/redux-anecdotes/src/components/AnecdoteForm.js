42b90a5a8c7a456f92f94ca7f41ceedf005207ad
Simon Greenwood <simonrobertgreenwood@gmail.com>
2022-12-30 08:36:26 +0000

    modified AnecdoteForm to use connect

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,27 +1,33 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch } from "react-redux";
+import { connect } from "react-redux";
+import { createNew } from "../reducers/anecdoteReducer";
+import { setNotificationWithTimeout } from "../reducers/notificationReducer";
 
-import { createNew } from '../reducers/anecdoteReducer'
-import { setNotificationWithTimeout } from '../reducers/notificationReducer'
-
-const AnecdoteForm = () => {
-  const dispatch = useDispatch()
-
+const AnecdoteForm = (props) => {
   const create = async (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createNew(content))
-    dispatch(setNotificationWithTimeout(`you created '${content}'`,5))
-  }
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    props.createNew(content);
+    props.setNotificationWithTimeout(`you created '${content}'`, 5);
+  };
 
   return (
     <div>
       <h2>create new</h2>
       <form onSubmit={create}>
-        <div><input name="anecdote"/></div>
+        <div>
+          <input name="anecdote" />
+        </div>
         <button type="submit">create</button>
       </form>
     </div>
-  )
-}
-export default AnecdoteForm
+  );
+};
+
+const mapDispatchToProps = {
+  createNew,
+  setNotificationWithTimeout,
+};
+export default connect(null, mapDispatchToProps)(AnecdoteForm);
+
