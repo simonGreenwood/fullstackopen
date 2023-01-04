803895fbeed63bfffeecfb81104a056faa7838fb
Simon Greenwood <simonrobertgreenwood@gmail.com>
2023-01-04 08:01:08 +0000

    added webpack dev server and styles

--- part7/webpack-part7/src/index.js
+++ part7/webpack-part7/src/index.js
@@ -1,3 +1,10 @@
-const hello = name => {
-console.log(`hello ${name}`)
-}
+import 'core-js/stable/index.js'
+import 'regenerator-runtime/runtime.js'
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
