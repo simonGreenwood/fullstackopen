95b168bc1410da99bf70d15ca4ab7dc6bfe3e37c
Simon Greenwood <simonrobertgreenwood@gmail.com>
2023-01-04 08:25:10 +0000

    added promise polyfill

--- part7/webpack-part7/src/index.js
+++ part7/webpack-part7/src/index.js
@@ -7,4 +7,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import PromisePolyfill from 'promise-polyfill'
+if (!window.Promise) {
+  window.Promise = PromisePolyfill
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
