71b5154a395b172b0321997d094aaab72d702344
Simon Greenwood <simonrobertgreenwood@gmail.com>
2023-01-04 08:16:11 +0000

    added different backend urls

--- part7/webpack-part7/src/App.js
+++ part7/webpack-part7/src/App.js
@@ -1,21 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    axios.get(url).then(response => {
+      setNotes(response.data)
+    })
+  }, [url])
+  return notes
+}
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const [values, setValues] = useState()
+  const [counter, setCounter] = useState(0)
+  const [values, setValues] = useState([])
+  const url = 'https://obscure-harbor-49797.herokuapp.com/api/notes'
+  const notes = useNotes(url)
 
   const handleClick = () => {
-    setCounter(counter + 1);
+    setCounter(counter + 1)
     setValues(values.concat(counter))
-  };
+  }
+
   return (
     <div className="container">
       hello webpack {counter} clicks
-      <button onClick={() => handleClick()}>
-        +1
-      </button>
+      <button onClick={handleClick}>press</button>
+      <div>{notes.length} notes on server {url}</div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
