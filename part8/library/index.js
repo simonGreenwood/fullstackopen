8fb5c31f9079f1f1228046f5bf9457ed44c80aa5
Simon Greenwood <simonrobertgreenwood@gmail.com>
2023-01-22 11:56:29 +0000

    exercise 8.4, modified allBooks query so you can specift an author

--- part8/library/index.js
+++ part8/library/index.js
@@ -96,7 +96,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 
@@ -112,7 +112,7 @@ const typeDefs = gql`
     name: String!
     id: ID!
     born: Int
-    bookCount: Int
+    bookCount: Int!
   }
 `
 
@@ -120,7 +120,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      }
+      return books.filter((book) => book.author === args.author)
+    },
     allAuthors: () => authors,
   },
   Author: {
