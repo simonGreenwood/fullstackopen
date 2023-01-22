1a82a8a2803e1b159d17e776baae195dda599886
Simon Greenwood <simonrobertgreenwood@gmail.com>
2023-01-22 12:17:54 +0000

    exercise 8.5, added the genre filter

--- part8/library/index.js
+++ part8/library/index.js
@@ -96,7 +96,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 
@@ -121,10 +121,19 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
+      if (!args.author && !args.genre) {
         return books
       }
-      return books.filter((book) => book.author === args.author)
+      if (args.author && !args.genre) {
+        return books.filter((book) => book.author === args.author)
+      }
+      if (!args.author && args.genre) {
+        return books.filter((book) => book.genres.includes(args.genre))
+      }
+      return books.filter(
+        (book) =>
+          book.author === args.author && book.genres.includes(args.genre)
+      )
     },
     allAuthors: () => authors,
   },
