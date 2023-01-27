271d0dba23471875edc50cb930d37421e99722dd
Simon Greenwood <simonrobertgreenwood@gmail.com>
2023-01-27 08:08:59 +0000

    created new react app and added graphql client to it

---
+++ part8/phonebook/phonebook-frontend/src/index.js
@@ -0,0 +1,30 @@
+import ReactDOM from "react-dom/client"
+import App from "./App"
+
+import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client"
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "http://localhost:4000",
+  }),
+})
+
+const query = gql`
+  query {
+    allPersons {
+      name
+      phone
+      address {
+        street
+        city
+      }
+      id
+    }
+  }
+`
+client.query({ query }).then((response) => {
+  console.log(response.data)
+})
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
