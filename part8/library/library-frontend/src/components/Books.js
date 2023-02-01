02e83be1a7d0360fabcd0b064cf0242a5939dc7a
Simon Greenwood <simonrobertgreenwood@gmail.com>
2023-02-01 07:54:55 +0000

    done with exercise 8.9

--- part8/library/library-frontend/src/components/Books.js
+++ part8/library/library-frontend/src/components/Books.js
@@ -1,10 +1,14 @@
+import { useQuery } from "@apollo/client"
+import { ALL_BOOKS } from "../queries"
 const Books = (props) => {
+  const books = useQuery(ALL_BOOKS)
   if (!props.show) {
     return null
   }
 
-  const books = []
-
+  if (books.loading) {
+    return <h1>Loading...</h1>
+  }
   return (
     <div>
       <h2>books</h2>
@@ -16,7 +20,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {books.data.allBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
