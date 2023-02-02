36f99093c085d575fc1b8921ed941c521123f5c8
Simon Greenwood <simonrobertgreenwood@gmail.com>
2023-02-02 08:04:17 +0000

    not done yet with 8.10

--- part8/library/library-frontend/src/components/NewBook.js
+++ part8/library/library-frontend/src/components/NewBook.js
@@ -1,21 +1,33 @@
 import { useState } from "react"
 
+import { useMutation } from "@apollo/client"
+import { ADD_BOOK, ALL_AUTHORS, ALL_BOOKS } from "../queries"
+
 const NewBook = (props) => {
   const [title, setTitle] = useState("")
   const [author, setAuthor] = useState("")
-  const [published, setPublished] = useState("")
+  const [published, setPublished] = useState(null)
   const [genre, setGenre] = useState("")
   const [genres, setGenres] = useState([])
 
+  const [addBook] = useMutation(ADD_BOOK, {
+    refetchQueries: [{ query: [ALL_BOOKS, ALL_AUTHORS] }],
+  })
   if (!props.show) {
     return null
   }
 
   const submit = async (event) => {
     event.preventDefault()
-
-    console.log("add book...")
-
+    console.log(title, author, published, genres)
+    addBook({
+      variables: {
+        title,
+        published,
+        author,
+        genres,
+      },
+    })
     setTitle("")
     setPublished("")
     setAuthor("")
@@ -50,7 +62,7 @@ const NewBook = (props) => {
           <input
             type="number"
             value={published}
-            onChange={({ target }) => setPublished(target.value)}
+            onChange={({ target }) => setPublished(Number(target.value))}
           />
         </div>
         <div>
--- part8/library/library-frontend/src/queries.js
+++ part8/library/library-frontend/src/queries.js
@@ -18,3 +18,21 @@ export const ALL_BOOKS = gql`
     }
   }
 `
+export const ADD_BOOK = gql`
+  mutation (
+    $title: String!
+    $published: Int!
+    $author: String!
+    $genres: [String!]!
+  ) {
+    addBook(
+      title: $title
+      published: $published
+      author: $author
+      genres: $genres
+    ) {
+      title
+      author
+    }
+  }
+`
