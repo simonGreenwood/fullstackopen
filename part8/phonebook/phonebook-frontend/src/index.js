653a677ceadbf1ef19e8cf333c34a02586ec29c2
Simon Greenwood <simonrobertgreenwood@gmail.com>
2023-02-28 08:53:27 +0000

    added the token to header

--- part8/phonebook/phonebook-frontend/src/index.js
+++ part8/phonebook/phonebook-frontend/src/index.js
@@ -3,16 +3,27 @@ import App from "./App"
 
 import {
   ApolloClient,
-  ApolloProvider,
-  HttpLink,
   InMemoryCache,
+  ApolloProvider,
+  createHttpLink
 } from "@apollo/client"
+import { setContext } from "@apollo/client/link/context"
 
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem("phonenumbers-user-token")
+  return {  
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    }
+  }
+})
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000",
+})
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: "http://localhost:4000",
-  }),
+  link: authLink.concat(httpLink)
 })
 
 ReactDOM.createRoot(document.getElementById("root")).render(
