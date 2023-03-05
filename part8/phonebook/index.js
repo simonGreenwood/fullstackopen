755ad3e0141d98b8c24b6c1ec83928cf2ebba7ed
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-05 08:15:34 +0000

    completed refactoring backend to use express for subscriptions

--- part8/phonebook/index.js
+++ part8/phonebook/index.js
@@ -1,12 +1,21 @@
 const { ApolloServer } = require("@apollo/server")
-const { startStandaloneServer } = require("@apollo/server/standalone")
-const mongoose = require("mongoose")
-mongoose.set("strictQuery", false)
-const Person = require("./models/person")
-const { GraphQLError } = require("graphql")
+const { expressMiddleware } = require("@apollo/server/express4")
+const {ApolloServerPluginDrainHttpServer} = require("@apollo/server/plugin/drainHttpServer") //prettier-ignore
+const { makeExecutableSchema } = require("@graphql-tools/schema")
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const http = require("http")
+
 require("dotenv").config()
 const jwt = require("jsonwebtoken")
+
+const typeDefs = require("./schema")
+const resolvers = require("./resolvers")
+
 const User = require("./models/user")
+
+const mongoose = require("mongoose")
 const MONGODB_URI = process.env.MONGODB_URI
 console.log("connecting to", MONGODB_URI)
 mongoose
@@ -18,136 +27,41 @@ mongoose
     console.log("error connecting to mongodb:", error.message)
   })
 
-const resolvers = {
-  Query: {
-    personCount: () => Person.collection.countDocuments(),
-    allPersons: async (root, args) => {
-      if (!args.phone) {
-        return await Person.find({})
-      }
-      return await Person.find({ phone: { $exists: args.phone === "YES" } })
-    },
-    findPerson: async (root, args) => await Person.findOne({ name: args.name }),
-    me: (root, args, context) => {
-      return context.currentUser
-    },
-  },
-  Person: {
-    address: (root) => {
-      return {
-        street: root.street,
-        city: root.city,
-      }
-    },
-  },
-  Mutation: {
-    addPerson: async (root, args, { currentUser }) => {
-      console.log(currentUser)
-      const person = new Person({ ...args })
-      if (!currentUser) {
-        throw new GraphQLError("not authenticated", {
-          extensions: {
-            code: "BAD_USER_INPUT",
-          },
-        })
-      }
-      try {
-        await person.save()
-        currentUser.friends = currentUser.friends.concat(person)
-        await currentUser.save()
-      } catch (error) {
-        throw new GraphQLError("Saving user failed", {
-          extensions: {
-            code: "BAD_USER_INPUT",
-            invalidArgs: args.name,
-            error,
-          },
-        })
-      }
-      return person
-    },
-    editNumber: async (root, args) => {
-      const person = await Person.findOne({ name: args.name })
-      person.phone = args.phone
-      try {
-        await person.save()
-      } catch (error) {
-        throw new GraphQLError("Saving number failed", {
-          extensions: {
-            code: "BAD_USER_INPUT",
-            invalidArgs: args.name,
-            error,
-          },
-        })
-      }
-      return person
-    },
-    createUser: async (root, args) => {
-      const user = new User({ username: args.username })
+const start = async () => {
+  const app = express()
+  const httpServer = http.createServer(app)
 
-      return user.save().catch((error) => {
-        throw new GraphQLError("Creating the user failed", {
-          extensions: {
-            code: "BAD_USER_INPUT",
-            invalidArgs: args.name,
-            error,
-          },
-        })
-      })
-    },
-    login: async (root, args) => {
-      const user = await User.findOne({ username: args.username })
-      if (!user || args.password !== "secret") {
-        throw new GraphQLError("wrong credentials", {
-          extensions: {
-            code: "BAD_USER_INPUT",
-          },
-        })
-      }
-      const userForToken = {
-        username: user.username,
-        id: user._id,
-      }
-      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
-    },
-    addAsFriend: async (root, args, { currentUser }) => {
-      const isFriend = (person) =>
-        currentUser.friends
-          .map((f) => f._id.toString())
-          .includes(person._id.toString())
+  const server = new ApolloServer({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  })
+  await server.start()
 
-      if (!currentUser) {
-        throw new GraphQLError("wrong credentials", {
-          extensions: { code: "BAD_USER_INPUT" },
-        })
-      }
-      const person = await Person.findOne({ name: args.name })
-      if (!isFriend(person)) {
-        currentUser.friends = currentUser.friends.concat(person)
-      }
-      await currentUser.save()
-      return currentUser
-    },
-  },
+  app.use(
+    "/",
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        const auth = req ? req.headers.authorization : null
+        if (auth && auth.startsWith("Bearer ")) {
+          const decodedToken = jwt.verify(
+            auth.substring(7),
+            process.env.JWT_SECRET
+          )
+          const currentUser = await User.findById(decodedToken.id).populate(
+            "friends"
+          )
+          return { currentUser }
+        }
+      },
+    })
+  )
+
+  const PORT = 4000
+  httpServer.listen(PORT, () => {
+    console.log(`Server is now runnign on http://localhost:${PORT}`)
+  })
 }
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
-
-startStandaloneServer(server, {
-  listen: { port: 4000 },
-  context: async ({ req, res }) => {
-    const auth = req ? req.headers.authorization : null
-    if (auth && auth.startsWith("Bearer ")) {
-      const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
-      const currentUser = await User.findById(decodedToken.id).populate(
-        "friends"
-      )
-      return { currentUser }
-    }
-  },
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`)
-})
+start()
