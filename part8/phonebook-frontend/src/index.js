bc122f94358d71bbf2481309c29318d7c77650d5
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-05 09:01:03 +0000

    adding subscriptions to client

--- part8/phonebook-frontend/src/index.js
+++ part8/phonebook-frontend/src/index.js
@@ -1,33 +1,52 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from "react-dom/client"
+import App from "./App"
 
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+  split,
+} from "@apollo/client"
+import { setContext } from "@apollo/client/link/context"
+
+import { getMainDefinition } from "@apollo/client/utilities"
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions"
+import { createClient } from "graphql-ws"
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("phonenumbers-user-token");
+  const token = localStorage.getItem("phonenumbers-user-token")
   return {
     headers: {
       ...headers,
       authorization: token ? `Bearer ${token}` : null,
     },
-  };
-});
+  }
+})
 const httpLink = createHttpLink({
   uri: "http://localhost:4000",
-});
+})
+
+const wsLink = new GraphQLWsLink(createClient({ url: "ws://localhost:4000" }))
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    )
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink),
-});
+  link: splitLink,
+})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>
-);
+)
