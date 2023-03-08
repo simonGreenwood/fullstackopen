86e81d248177bfc448d7c4441cc72c914fd41dbd
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-08 07:46:20 +0000

    done subscriptions now finishing the cache

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 import { useMutation } from "@apollo/client"
 import { ADD_BOOK, ALL_AUTHORS, ALL_BOOKS } from "../queries"
-
+import { updateCache } from "../App"
 const NewBook = (props) => {
   const [title, setTitle] = useState("")
   const [author, setAuthor] = useState("")
@@ -16,11 +16,8 @@ const NewBook = (props) => {
       console.log(error.graphQLErrors[0].message)
     },
     update: (cache, response) => {
-      cache.updateQuery({ query: ALL_BOOKS }, ({ allBooks }) => {
-        return {
-          allBooks: allBooks.concat(response.data.allBooks),
-        }
-      })
+      console.log(response.data)
+      updateCache(cache, { query: ALL_BOOKS }, response.data.addBook)
     },
   })
   if (!props.show) {
