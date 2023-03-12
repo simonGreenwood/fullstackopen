53e4d73d76d7c87ace24cd576f9441dcc4f0e3bf
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-12 11:51:12 +0000

    done with the bmi calculator

---
+++ part9/bmi/bmiCalculator.ts
@@ -0,0 +1,13 @@
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi: number = weight/((height/100)**2)
+  if ( bmi < 20) {
+    return "Underweight (unhealthy weight)" 
+  } 
+  else if (bmi < 25) {
+    return "Normal (healthy weight)"
+  }
+  else {
+    return "Overweight (unhealthy weight)"
+  }
+}
+console.log(calculateBmi(178,9))
