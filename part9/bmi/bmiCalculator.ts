f5c9cc1dbb3d42d03c0626285eb24a8b7bb297eb
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-12 12:21:50 +0000

    done with exercise 9.2

--- part9/bmi/bmiCalculator.ts
+++ part9/bmi/bmiCalculator.ts
@@ -1,13 +1,11 @@
 const calculateBmi = (height: number, weight: number): string => {
-  const bmi: number = weight/((height/100)**2);
-  if ( bmi < 20) {
+  const bmi: number = weight / (height / 100) ** 2;
+  if (bmi < 20) {
     return "Underweight (unhealthy weight)";
-  } 
-  else if (bmi < 25) {
+  } else if (bmi < 25) {
     return "Normal (healthy weight)";
-  }
-  else {
+  } else {
     return "Overweight (unhealthy weight)";
   }
-}
-console.log(calculateBmi(178,9));
+};
+console.log(calculateBmi(178, 9));
--- part9/bmi/exerciseCalculator.ts
+++ part9/bmi/exerciseCalculator.ts
@@ -1,22 +1,28 @@
-const calculateExercises = (hours: number[], target:number) => {
-  const average: number = hours.reduce((a,b) => a + b, 0)/hours.length;
+const calculateExercises = (hours: number[], target: number) => {
+  const average: number = hours.reduce((a, b) => a + b, 0) / hours.length;
 
-  const rating: number = 1
+  let rating: number = 0;
+  let ratingDescription: string = "";
+
+  if (average > target) {
+    rating = 3;
+    ratingDescription = "very good, well done";
+  } else if (average > target * 0.8) {
+    rating = 2;
+    ratingDescription = "not too bad but could be better";
+  } else {
+    rating = 1;
+    ratingDescription = "not good enough, try again next week";
+  }
+
   return {
     periodLength: hours.length,
-    trainingDays: hours.filter(day => day > 0).length,
+    trainingDays: hours.filter((day) => day > 0).length,
     success: average >= target,
-    rating: 2,
-    ratingDescription: 'not too bad but could be better',
-    target: target,
-    average: average
-  }
-}
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1],2));
-/*{ periodLength: 7,
-  trainingDays: 5,
-  success: false,
-  rating: 2,
-  ratingDescription: 'not too bad but could be better',
-  target: 2,
-  average: 1.9285714285714286 }*/
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
