b96f5a59d196bb5c028405221a060f2dae08878d
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-12 11:19:12 +0000

    started the typescript part

---
+++ part9/firststeps/multiplier.ts
@@ -0,0 +1,14 @@
+type Operation = 'multiply' | 'add' | 'divide'
+type Result = string | number
+const calculator = (a: number, b: number, op: Operation): Result => {
+  if (op === 'multiply') {
+    return a * b;
+  } else if (op === 'add') {
+    return a + b;
+  } else if (op === 'divide') {
+    if (b === 0) return 'can\'t divide by 0!';
+    return a / b;
+  }
+}
+
+console.log(calculator(1,2,"add"))
