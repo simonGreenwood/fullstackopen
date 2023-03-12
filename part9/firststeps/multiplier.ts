0314256f95385b45679a1886a496fe4b1434c8ea
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-12 11:36:13 +0000

    everything until 9.1 done

--- part9/firststeps/multiplier.ts
+++ part9/firststeps/multiplier.ts
@@ -1,14 +1,34 @@
-type Operation = 'multiply' | 'add' | 'divide'
-type Result = string | number
-const calculator = (a: number, b: number, op: Operation): Result => {
-  if (op === 'multiply') {
-    return a * b;
-  } else if (op === 'add') {
-    return a + b;
-  } else if (op === 'divide') {
-    if (b === 0) return 'can\'t divide by 0!';
-    return a / b;
+interface MultiplyValues {
+  value1: number;
+  value2: number;
+}
+
+const parseArguments = (args: string[]): MultiplyValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
   }
 }
 
-console.log(calculator(1,2,"add"))
+const multiplicator = (a: number, b: number, printText: string) => {
+  console.log(printText,  a * b);
+}
+
+
+try {
+  const { value1, value2 } = parseArguments(process.argv);
+  multiplicator(value1, value2, `Multiplied ${value1} and ${value2}, the result is:`);
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
