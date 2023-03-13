640c1687bfbfdfdf29617175433524dcd7d5f4db
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-13 07:52:17 +0000

    added the isNotNumber helper function

--- part9/bmi/bmiCalculator.ts
+++ part9/bmi/bmiCalculator.ts
@@ -18,7 +18,7 @@ const parseArguments = (args: string[]): BmiValues => {
   if (args.length < 4) throw new Error("Not enough arguments");
   if (args.length > 4) throw new Error("Too many arguments");
 
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+  if (!isNotNumber(args[2]) && !isNotNumber(args[3])) {
     return {
       height: Number(args[2]),
       weight: Number(args[3]),
