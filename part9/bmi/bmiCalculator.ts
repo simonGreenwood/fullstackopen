0b609afacc78722b29b79b57061760e5dee6e7ca
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-13 07:41:02 +0000

    added command line arguments to the bmi calculator

--- part9/bmi/bmiCalculator.ts
+++ part9/bmi/bmiCalculator.ts
@@ -8,4 +8,31 @@ const calculateBmi = (height: number, weight: number): string => {
     return "Overweight (unhealthy weight)";
   }
 };
-console.log(calculateBmi(178, 9));
+interface MultiplyValues {
+  height: number;
+  weight: number;
+}
+const parseArguments = (args: string[]) => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: args[2],
+      weight: args[3],
+    };
+  } else {
+    throw new Error("Values aren't numbers!");
+  }
+};
+
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(Number(height), Number(weight)));
+} catch (error: unknown) {
+  let errorMessage = "something went wrong";
+  if (error instanceof Error) {
+    errorMessage += error;
+  }
+  console.log(errorMessage);
+}
