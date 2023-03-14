503c442c7367f4272ec0ead1b3a7737cf552e31c
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-14 18:03:35 +0000

    done with exercise 9.4

--- part9/bmi/utils.ts
+++ part9/bmi/utils.ts
@@ -1,7 +1,5 @@
 export const isNotNumber = (argument: any): boolean => isNaN(Number(argument));
-export const containsNotNumber = (argument: any[]) => {
-  return argument.map((e) => isNaN(Number(e))).includes(false);
-};
+
 export const handleError = (error: unknown): string => {
   let errorMessage = "something went wrong: ";
   if (error instanceof Error) {
