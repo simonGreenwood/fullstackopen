5d5fceac1c03be51815fd8686371913f89bfc2e2
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-15 07:55:17 +0000

    fixed eslint

--- part9/first-steps/index.ts
+++ part9/first-steps/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import { calculator } from "./calculator";
 const app = express();
 
 app.get("/ping", (_req, res) => {
   res.send("pong");
 });
 
+app.post("/calculate", (req, res) => {
+  const { value1, value2, op } = req.body;
+  const result = calculator(value1, value2, op);
+  res.send({ result });
+});
 const PORT = 3000;
 
 app.listen(PORT, () => {
