0e2782c1c762509a035aed304649f43dcdeae785
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-16 19:14:35 +0000

    almost done with 9.9 havent done cors yet

--- part9/patientor/patientor-backend/index.ts
+++ part9/patientor/patientor-backend/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/api/ping", (_req, res) => {
   console.log("ping");
