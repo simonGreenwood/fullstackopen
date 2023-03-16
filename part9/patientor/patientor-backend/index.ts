165ed7d8b2e3809a2a0163b012de5da3dbbc95c1
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-16 08:04:39 +0000

    9.8

---
+++ part9/patientor/patientor-backend/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+
+app.get("/ping", (_req, res) => {
+  console.log("ping");
+  res.send("pong");
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
