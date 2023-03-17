9328a288e7b3d0a329db06dc65e47d093b954123
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-17 08:00:56 +0000

    done with implementing functionality, node/json modules and utility types

--- part9/flight-diary/src/index.ts
+++ part9/flight-diary/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import diaryRouter from "./routes/diaries";
 const app = express();
 app.use(express.json());
+app.use("/api/diaries", diaryRouter);
 
 const PORT = 3000;
 
@@ -9,8 +10,6 @@ app.get("/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
 });
-
-app.use("/api/diaries", diaryRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
