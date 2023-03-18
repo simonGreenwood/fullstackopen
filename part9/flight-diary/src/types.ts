047218173688f41901d01ccc0bc3266f10379704
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-18 08:24:24 +0000

    done with enums etc.

--- part9/flight-diary/src/types.ts
+++ part9/flight-diary/src/types.ts
@@ -1,6 +1,16 @@
-export type Weather = "sunny" | "rainy" | "cloudy" | "windy" | "stormy";
-export type Visibility = "great" | "good" | "ok" | "poor";
-
+export enum Weather {
+  Sunny = "sunny",
+  Rainy = "rainy",
+  Cloudy = "cloudy",
+  Stormy = "stormy",
+  Windy = "windy",
+}
+export enum Visibility {
+  Great = "great",
+  Good = "good",
+  Ok = "ok",
+  Poor = "poor",
+}
 export interface DiaryEntry {
   id: number;
   date: string;
@@ -8,5 +18,6 @@ export interface DiaryEntry {
   visibility: Visibility;
   comment?: string;
 }
+
 export type NonSensitiveDiaryEntry = Omit<DiaryEntry, "comment">;
 export type NewDiaryEntry = Omit<DiaryEntry, "id">;
