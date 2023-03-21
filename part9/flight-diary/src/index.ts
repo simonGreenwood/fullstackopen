a4c53b7970066542a75dd13306b671dc51fb186c
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-21 19:20:56 +0000

    modified backend to allow for CORS

--- part9/flight-diary/src/index.ts
+++ part9/flight-diary/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import diaryRouter from "./routes/diaries";
+import cors from "cors";
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3001;
 
