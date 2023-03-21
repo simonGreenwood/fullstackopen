04f4a45ec5e2a451bc9d6558e0c89c95f8bf442c
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-21 19:00:42 +0000

    done with the non exercise parts for part d

---
+++ part9/state/src/App.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { Note } from "./types";
+import { getAllNotes, createNote } from "./noteService";
+const App = () => {
+  const [notes, setNotes] = useState<Note[]>([{ id: 1, content: "testing" }]);
+  const [newNote, setNewNote] = useState("");
+
+  useEffect(() => {
+    getAllNotes().then((data) => {
+      setNotes(data);
+    });
+  }, []);
+
+  const noteCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    createNote({ content: newNote }).then((data) => {
+      setNotes(notes.concat(data));
+    });
+
+    setNewNote("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={noteCreation}>
+        <input
+          value={newNote}
+          onChange={(event) => setNewNote(event.target.value)}
+        />
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>{note.content}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default App;
