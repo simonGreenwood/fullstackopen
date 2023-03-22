d1c87e60b7cbafc8edd161c6031a68e1df902c2a
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-22 08:02:20 +0000

    almost done with 9.17

--- part9/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary-frontend/src/App.tsx
@@ -1,20 +1,37 @@
 import { DiaryEntry } from "./types";
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import Diary from "./components/Diary";
+import NewDiary from "./components/NewDiary";
+interface ValidationError {
+  message: string;
+  errors: Record<string, string[]>;
+}
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
-
+  const [errorMessage, setErrorMessage] = useState("");
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/api/diaries")
-      .then((response) => setDiaries(response.data));
+    try {
+      axios
+        .get("http://localhost:3001/api/diaries")
+        .then((response) => setDiaries(response.data));
+    } catch (error) {
+      if (axios.isAxiosError<ValidationError, Record<string, unknown>>(error)) {
+        console.log(error.status);
+        console.log(error.response);
+      } else {
+        console.error(error);
+      }
+    }
   });
   return (
     <div>
-      <h1>Diary Entries</h1>
+      <h3>Add new entry</h3>
+      <div style={{ color: "red" }}>{errorMessage}</div>
+      <NewDiary />
+      <h3>Diary Entries</h3>
       {diaries.map((diary) => (
-        <Diary diary={diary} />
+        <Diary diary={diary} key={diary.id} />
       ))}
     </div>
   );
