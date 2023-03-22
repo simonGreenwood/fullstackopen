2ac1b797509fa04d48d6414071f3f0d8586911e5
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-22 19:55:51 +0000

    done with services

--- part9/flight-diary-frontend/src/services/diaryService.ts
+++ part9/flight-diary-frontend/src/services/diaryService.ts
@@ -1,1 +1,11 @@
-export {};
+import { DiaryEntry, NewDiaryEntry } from "../types";
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001/api/diaries";
+export const createDiary = (object: NewDiaryEntry) => {
+  return axios.get<DiaryEntry>(baseUrl).then((response) => response.data);
+};
+
+export const getDiaries = () => {
+  return axios.get<DiaryEntry[]>(baseUrl).then((response) => response.data);
+};
