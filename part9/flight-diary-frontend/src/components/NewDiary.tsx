51b64322e5ae22e291e86f9ca9c5f5856b2dc0a8
simon greenwood <simonrobertgreenwood@gmail.com>
2023-03-22 08:02:20 +0000

    almost done with 9.17

---
+++ part9/flight-diary-frontend/src/components/NewDiary.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const NewDiary = () => {
+  const [date, setDate] = useState("");
+  const [visibility, setVisibility] = useState("");
+  const [weather, setWeather] = useState("");
+  const [comment, setComment] = useState("");
+
+  const submit = () => {
+    return null;
+  };
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <div>
+          date
+          <input value={date} onChange={(e) => setDate(e.target.value)} />
+        </div>
+        <div>
+          visibility
+          <input
+            value={visibility}
+            onChange={(e) => setVisibility(e.target.value)}
+          />
+        </div>
+        <div>
+          weather
+          <input value={weather} onChange={(e) => setWeather(e.target.value)} />
+        </div>
+        <div>
+          comment
+          <input value={comment} onChange={(e) => setComment(e.target.value)} />
+        </div>
+        <button type="submit">add</button>
+      </form>
+    </div>
+  );
+};
+export default NewDiary;
