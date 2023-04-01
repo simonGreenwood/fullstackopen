f355209b94b5ef53baf66963ebf9561d8f14f826
simon greenwood <simonrobertgreenwood@gmail.com>
2023-04-01 08:23:08 +0100

    created the extra types for 9.22

--- part9/patientor/patientor-backend/src/types.ts
+++ part9/patientor/patientor-backend/src/types.ts
@@ -1,11 +1,45 @@
+export interface Diagnosis {
+  code: string;
+  name: string;
+  latin?: string;
+}
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+}
+
+export interface HospitalEntry {
+  type: "Hospital";
+}
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 export enum Gender {
   Male = "male",
   Female = "female",
   Other = "other",
 }
 
-//eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
 export interface Patient {
   id: string;
   name: string;
@@ -18,8 +52,3 @@ export interface Patient {
 
 export type NonSensitivePatient = Omit<Patient, "ssn" | "entries">;
 export type NewPatient = Omit<Patient, "id">;
-export interface Diagnosis {
-  code: string;
-  name: string;
-  latin?: string;
-}
