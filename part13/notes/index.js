9de9c93a2032c5f3c37b4010ff307e0cf59e1cde
simon greenwood <simonrobertgreenwood@gmail.com>
2023-05-29 07:32:44 +0100

    printed the sequelize objects

--- part13/notes/index.js
+++ part13/notes/index.js
@@ -31,8 +31,12 @@ Note.init(
     modelName: "note",
   }
 )
+
+Note.sync()
 app.get("/api/notes", async (req, res) => {
   const notes = await Note.findAll()
+  console.log(notes.map((n) => n.toJSON))
+  console.log(JSON.stringify(notes))
   res.json(notes)
 })
 
@@ -44,6 +48,28 @@ app.post("/api/notes", async (req, res) => {
     return res.status(400).json({ error })
   }
 })
+
+app.get("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id)
+  if (note) {
+    console.log(note.toJSON())
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.put("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id)
+  if (note) {
+    note.important = req.body.important
+    await note.save()
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
