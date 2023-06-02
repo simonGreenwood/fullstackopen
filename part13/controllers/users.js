4e9838c2d987f4832ca05eed2b27873bf4fcd6d6
simon greenwood <simonrobertgreenwood@gmail.com>
2023-06-02 08:25:53 +0100

    finished the section on many to many relationships

--- part13/controllers/users.js
+++ part13/controllers/users.js
@@ -1,56 +1,65 @@
-const router = require("express").Router()
-const { User, Note } = require("../models")
-const { SECRET } = require("../util/config")
-const { tokenExtractor } = require("../util/middleware")
+const router = require("express").Router();
+const { User, Note, Team } = require("../models");
+const { SECRET } = require("../util/config");
+const { tokenExtractor } = require("../util/middleware");
 const isAdmin = async (req, res, next) => {
-  const user = await User.findByPk(req.decodedToken.id)
+  const user = await User.findByPk(req.decodedToken.id);
   if (!user.admin) {
-    return res.status(401).json({ error: "operation not allowed" })
+    return res.status(401).json({ error: "operation not allowed" });
   }
-  next()
-}
+  next();
+};
 
 router.put("/:username", tokenExtractor, isAdmin, async (req, res) => {
   const user = await User.findOne({
     where: {
       username: req.params.username,
     },
-  })
+  });
 
   if (user) {
-    user.disabled = req.body.disabled
-    await user.save()
-    res.json(user)
+    user.disabled = req.body.disabled;
+    await user.save();
+    res.json(user);
   } else {
-    res.status(400).end()
+    res.status(400).end();
   }
-})
+});
 router.get("/", async (req, res) => {
   const users = await User.findAll({
-    include: {
-      model: Note,
-      attributes: { exclude: ["userId"] },
-    },
-  })
-  res.json(users)
-})
+    include: [
+      {
+        model: Note,
+        attributes: { exclude: ["userId"] },
+      },
+      {
+        model: Team,
+        attributes: ["name", "id"],
+        through: {
+          attributes: [],
+        },
+      },
+    ],
+  });
+  res.json(users);
+});
 
 router.post("/", async (req, res) => {
   try {
-    const user = await User.create(req.body)
-    res.json(user)
+    const user = await User.create(req.body);
+    res.json(user);
   } catch (error) {
-    return res.status(400).json({ error })
+    return res.status(400).json({ error });
   }
-})
+});
 
 router.get("/:id", async (req, res) => {
-  const user = await User.findByPk(req.params.id)
+  const user = await User.findByPk(req.params.id);
   if (user) {
-    res.json(user)
+    res.json(user);
   } else {
-    res.status(404).end()
+    res.status(404).end();
   }
-})
+});
 
-module.exports = router
+module.exports = router;
